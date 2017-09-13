53f92128efc1652f1103fb886ef65b1fdef019f8
Paymon <paymon@geeks.ltd.uk>
2017-09-13 17:55:57 +0100

    Reference update

--- Shared/Carousel.cs
+++ Shared/Carousel.cs
@@ -1,6 +1,7 @@
 ﻿namespace Zebble.Plugin
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -161,6 +162,11 @@
             public Task<Slide> Add(View slide) => Carousel.AddSlide(slide);
             public int Count => Carousel.SlidesContainer.CurrentChildren.Count();
             public bool Zoomed => Carousel.SlidesContainer.CurrentChildren.Any(x => !(x as ScrollView).Zoom.AlmostEquals(1, 0.1f));
+
+            public virtual async Task AddRange<T>(IEnumerable<T> slides) where T : View
+            {
+                foreach (var s in slides) await Add(s);
+            }
         }
 
         public class Slide : ScrollView { }
