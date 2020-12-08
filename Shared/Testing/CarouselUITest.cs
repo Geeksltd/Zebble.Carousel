c6ec0ba3b8cfbb83b8d3479e5600fd34ef2ef817
Mohammad Sadegh Zojaji <ms.zojaji@gmail.com>
2020-12-08 16:35:17 +0330

    Add testing extensions

---
+++ Shared/Testing/CarouselUITest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zebble;
+
+namespace Zebble.Plugin
+{
+    public static class CarouselUITestExtention
+    {
+        public static void SwipeCarousel(this IBaseUITest @this, Direction direction = Direction.Left, View thisCarousel = null)
+        {
+            var delay = 200;
+
+#if ANDROID
+            delay = 500;
+#endif
+
+            @this.Delay(delay);
+
+            Carousel carousel;
+            if (thisCarousel == null) carousel = @this.Find<Carousel>();
+            else carousel = thisCarousel as Carousel;
+
+            if (direction == Direction.Left) carousel.Next(animate: false);
+            else if (direction == Direction.Right) carousel.Previous(animate: false);
+        }
+    }
+}
