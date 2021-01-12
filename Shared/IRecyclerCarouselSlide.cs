22a1375b1197c2bfcf430ce399b4cc0affaa07c2
Mohammad Sadegh Shad <mohammadsadegh.shad@geeks.ltd.uk>
2021-01-12 18:08:54 +0330

    Update

--- Shared/IRecyclerCarouselSlide.cs
+++ Shared/IRecyclerCarouselSlide.cs
@@ -1,5 +1,7 @@
 ﻿namespace Zebble.Plugin
 {
+    using Olive;
+
     public interface IRecyclerCarouselSlide<TSource>
     {
         Bindable<TSource> Item { get; }
--- Shared/RecyclerCarouselSlide.cs
+++ Shared/RecyclerCarouselSlide.cs
@@ -1,5 +1,7 @@
 ﻿namespace Zebble.Plugin
 {
+    using Olive;
+
     public abstract class RecyclerCarouselSlide<TSource> : Stack, IRecyclerCarouselSlide<TSource>
     {
         public Bindable<TSource> Item { get; } = new Bindable<TSource>();
